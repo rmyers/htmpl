0320c70e5b1db08a32c95b3dee85b04bbba6d41e
Robert Myers <rmyers@anaconda.com>
2026-01-27 09:13:36 -0600

    feat: Adding exports to init file

--- src/htmpl/__init__.py
+++ src/htmpl/__init__.py
@@ -7,7 +7,44 @@ template strings for type-safe, composable HTML generation.
 
 from tdom import html
 
-__all__ = [
-    # TDOM re-export
-    "html",
-]
+__all__ = ["html"]
+
+try:
+    from .core import SafeHTML, render, render_html
+    from .assets import registry, component, Component, ComponentFunc
+    from .fastapi import (
+        use_bundles,
+        use_component,
+        use_form,
+        add_assets_routes,
+        ParsedForm,
+    )
+    from .forms import BaseForm, parse_form_errors
+
+    __all__ += [
+        # core functions
+        "SafeHTML",
+        "render",
+        "render_html",
+        # Asset management
+        "registry",
+        "component",
+        "Component",
+        "ComponentFunc",
+        # FastAPI dependencies
+        "use_bundles",
+        "use_component",
+        "use_form",
+        "add_assets_routes",
+        # Forms
+        "ParsedForm",
+        "BaseForm",
+        "parse_form_errors",
+    ]
+
+except ImportError:
+    # Prevent import errors from running the cli commands
+    # for example `uvx htmpl init` since we are not guarenteed to have FastAPI installed.
+    # probably we should log a message instead?
+    # TODO(rmyers): think about refactoring all external deps into a single module
+    pass
